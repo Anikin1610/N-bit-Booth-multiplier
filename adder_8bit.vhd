649e389ac923ad911fe1a0878d950a2be1a24981
Anirudh <ianirudh16@gmail.com>
2020-11-25 09:17:51 +0530

    Added Comments

--- adder_8bit.vhd
+++ adder_8bit.vhd
@@ -7,11 +7,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity adder_Nbit is
     generic ( N : integer := 8 );
-    Port ( ip1 : in STD_LOGIC_VECTOR (N - 1 downto 0);
-           ip2 : in STD_LOGIC_VECTOR (N - 1 downto 0);
-			  sub : in STD_LOGIC;
-           op : out STD_LOGIC_VECTOR (N - 1 downto 0);
-			  op_carry : out STD_LOGIC);
+    Port ( 	ip1 : in STD_LOGIC_VECTOR (N - 1 downto 0);
+           	ip2 : in STD_LOGIC_VECTOR (N - 1 downto 0);
+			sub : in STD_LOGIC;							
+           	op : out STD_LOGIC_VECTOR (N - 1 downto 0);
+			op_carry : out STD_LOGIC);
 end adder_Nbit;
 
 architecture adder_beh of adder_Nbit is
@@ -26,7 +26,6 @@ architecture adder_beh of adder_Nbit is
 	signal ip2_signed : STD_LOGIC_VECTOR(N - 1 downto 0);
 
 begin
-
 	xor_gen : for j in 0 to N - 1 generate
 		ip2_signed(j) <= ip2(j) xor sub;
 	end generate xor_gen;
@@ -34,7 +33,7 @@ begin
 	FA1: FullAdder_1bit port map(a => ip1(0), b => ip2_signed(0), cin => sub, cout => carry(0),sum => op(0));
 	FA2: for i in 1 to N - 2 generate
 	FA3: FullAdder_1bit port map (a => ip1(i), b => ip2_signed(i), cin => carry(i - 1), cout => carry(i), sum => op(i));
-		  end generate;
+		 end generate;
 	FA4: FullAdder_1bit port map(a => ip1(N - 1), b => ip2_signed(N - 1), cin => carry(N - 2), cout => op_carry, sum => op(N - 1));
 
 end adder_beh;
